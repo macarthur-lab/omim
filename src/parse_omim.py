9f46f0539097e177736defc563c9ecf0f24b3048
Daniel Birnbaum <daniel@MGH002911.local>
2016-07-20 11:29:27 -0400

    Minor code changes, re-generated OMIM files

--- src/parse_omim.py
+++ src/parse_omim.py
@@ -17,18 +17,18 @@ from collections import defaultdict
 def get_gene_thesaurus(filename):
 	"""
 	Returns mapping from gene symbol to list of synonymous, approved gene symbols 
-	ideally, there should only be one approved synonym for any given symbol, but the file has lines where
-	a non-approved symbol will appear in the approved column and thus will be mapped to itself
-	e.g. FRAXA NA
+	ideally, there should only be one approved synonym for any given symbol, but 
+	this is not always the case. For example, there are some symbols that are listed both
+	as approved symbols and as synonyms for a different approved symbol.
 	"""
 	thesaurus = defaultdict(list)
 	with open(filename, 'r') as lines:
 		for line in lines:
 			line = line.strip().split("\t")
 			approved = line[0]
-			synonyms = line[1].split(",")
 			thesaurus[approved].append(approved)
-			if synonyms != "NA":
+			if line[1] != "NA":
+				synonyms = line[1].split(",")
 				for word in synonyms:
 					thesaurus[word].append(approved)
 	return thesaurus
@@ -96,14 +96,15 @@ def main(args):
 			line = ['']*len(header.keys())
 			geneMap = g['geneMap']
 			genes = re.sub(' ', '', geneMap['geneSymbols']).split(',')
-			hgnc_genes = [','.join(t[gene]) if t[gene] else 'NA' for gene in genes]
-			if args.use and all(map(lambda g:g == 'NA', hgnc_genes)):
-				continue
+			hgnc_genes = [','.join(t[gene]) if len(t[gene]) > 0 else 'NA' for gene in genes] # attempt to match to HGNC-approved name
+
+			# skip entry if none of the gene names can be mapped to an HGNC-approved name 
+			if args.use and all(map(lambda g:g == 'NA', hgnc_genes)): continue
 
 			line[header['hgnc_synonyms']] = '|'.join(hgnc_genes)
-			tmp = reduce(lambda a, b: a + ',' + b, hgnc_genes)
-			hgnc_genes = set(filter(lambda x:x != 'NA', tmp.split(',')))
-			if not hgnc_genes: 
+			tmp = reduce(lambda a, b: a + ',' + b, hgnc_genes) # concatenate approved symbols
+			hgnc_genes = set(filter(lambda x:x != 'NA', tmp.split(','))) # split, filter NAs, re-concatenate to get hgnc_genes column
+			if len(hgnc_genes) == 0: 
 				hgnc_genes = 'NA'
 			else:
 				hgnc_genes = ','.join(hgnc_genes)
@@ -153,9 +154,7 @@ if __name__ == '__main__':
 	parser.add_argument('--hgnc', dest='hgnc', help='Path to gene thesaurus file.', required=True)
 	parser.add_argument('-o', '--output', dest='output', default=sys.stdout)
 	parser.add_argument('--chrom', help='Only get data for given chromosome.')
-	parser.add_argument('--use', action='store_true', help='Only output entries with a gene AND '
-														   'associated phenotype AND WHERE '
-														   'the gene can be matched to an HGNC-approved symbol.')
+	parser.add_argument('--use', action='store_true', help='Only output entries with a gene AND associated phenotype AND WHERE the gene can be matched to an HGNC-approved symbol.')
 	args = parser.parse_args()
 
 	main(args)
